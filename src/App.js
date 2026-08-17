8747c459d7884c7a95ff9496ea4a1352d806ef92
Tomas Gusstavsson <tomas.gustavsson@hiq.se>
2026-08-17 15:05:20 +0200

    lesson 10 router

--- src/App.js
+++ src/App.js
@@ -1,41 +1,31 @@
+import {Routes, Route, Outlet} from 'react-router-dom';
+import Home from "./routes/home/home.componet"
 
-import Directory from './components/directory/directory.componet.jsx';
-import './components/category.items/category-item.styels.scss';
-const App = () => {
+const Navigation = () => {
+return (
+ <div>
+  <div>
+    <h1>Navigation</h1>
+  </div>
+   <Outlet />
+ </div> 
+)
+};
 
-  const categories = [
-      {
-    "id": 1,
-    "title": "hats",
-    "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-  },
-  {
-    "id": 2,
-    "title": "jackets",
-    "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-  },
-  {
-    "id": 3,
-    "title": "sneakers",
-    "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-  },
-  {
-    "id": 4,
-    "title": "womens",
-    "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-  },
-  {
-    "id": 5,
-    "title": "mens",
-    "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-  }
-];
-
-
+const Shop = () => {
   return (
-    <div>
-      <Directory categories={categories} />
-    </div>
+      <h1>Shop</h1>
+  );
+}
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+         <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
   );
 };
 
