b8b6a8fb686670d2403d9bf439f807a1029b3d0e
tguss76 <tguss76@gmail.com>
2026-08-19 13:14:11 +0200

    lesson-10 navigation and routing

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-import {Routes, Route, Outlet} from 'react-router-dom';
-import Home from "./routes/home/home.componet"
-
-const Navigation = () => {
-return (
- <div>
-  <div>
-    <h1>Navigation</h1>
-  </div>
-   <Outlet />
- </div> 
-)
-};
+import Navigation from "./routes/navigation/navigation.compnet";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.componet";
 
 const Shop = () => {
-  return (
-      <h1>Shop</h1>
-  );
-}
+  return <h1>Shop</h1>;
+};
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<Navigation />}>
-         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
+      <Route path="/" element={<Navigation />}>
+        <Route index={true} element={<Home />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
---
+++ src/routes/navigation/navigation.compnet.jsx
@@ -0,0 +1,25 @@
+import {Outlet, Link } from "react-router-dom";
+import './navigation.styles.scss'
+import { Fragment } from "react";
+import {ReactComponent as CrownLogo} from '../../assets/crown.svg'
+
+
+const Navigation = () => {
+    return (
+        <Fragment >
+            <div className="navigation">
+                <Link className="logo-container" to="/">
+                    <CrownLogo className='logo'/>
+                </Link>
+            <div className="nav-links-container">
+                <Link className="nav-link" to='/shop'>
+                    SHOP
+                </Link>
+            </div>
+        </div>
+        <Outlet />
+        </Fragment>
+    );
+};
+
+export default Navigation
